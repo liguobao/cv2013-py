91f33995bdb9516a8511230cc96006d5df7abb4c
liguobao <codelover@qq.com>
2019-12-19 15:48:04 +0800

    完成保存文件到本地

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
-
+# 根据URL获取HTML
 def get_html(url):
     headers = {
         'Proxy-Connection': "keep-alive",
@@ -15,7 +16,7 @@ def get_html(url):
     response = requests.request("GET", url, headers=headers)
     return response.text
 
-
+# 获取一个paper url的内容，直接转成一个Python的字典
 def get_paper_detail(paper_url):
     paper_html = get_html(paper_url)
     paper_soup = BeautifulSoup(paper_html, "html.parser")
@@ -39,6 +40,7 @@ def get_paper_detail(paper_url):
     return None
 
 
+# 获取首页里面所有的链接
 def get_paper_urls(index_url):
     htmlSoup = BeautifulSoup(get_html(index_url), "html.parser")
     all_paper_dts = htmlSoup.find_all("dt")
@@ -48,19 +50,27 @@ def get_paper_urls(index_url):
         if a_paper:
             all_paper_urls.append(
                 "http://openaccess.thecvf.com/"+a_paper["href"])
+    return all_paper_urls
+
+# 将数据保存到json文件中
+def save_json(file_name, json_data):
+    with open(f'{file_name}.json', 'w', encoding='utf-8') as f:
+        json.dump(json_data, f, ensure_ascii=False, indent=4)
 
 
 index_url = "http://openaccess.thecvf.com/ICCV2013.py"
 all_paper_urls = get_paper_urls(index_url)
-
+print(f"get all_paper_urls finish, len:{len(all_paper_urls)}")
 papers = []
-
+crawl_index = 0
 for paper_url in all_paper_urls:
-    print(f"crawl {paper_url} start.")
+    print(f"crawl[{crawl_index}] {paper_url} start.")
     paper_detail = get_paper_detail(paper_url)
     if paper_detail is None:
         print(f"crawl {paper_url} fail.")
         continue
     papers.append(paper_detail)
-    print(f"crawl {paper_url} end.")
-print(papers)
+    print(f"crawl[{crawl_index}] {paper_url} end.")
+    crawl_index = crawl_index + 1
+save_json("icccv2013", papers)
+print(f"crawl papers finish, len:{len(papers)}")
